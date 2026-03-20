39049071b340561e2ec92a503ad76a2e0cfe24fd
jitendrax7 <rathourjitendrasingh472@gmail.com>
2026-03-20 23:19:18 +0530

    h

--- server/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { chatWithAI } from "../controllers/chatController.js";
-
-const chatRoutes = express.Router();
-
-chatRoutes.post("/chat", chatWithAI);
-
-export default chatRoutes;
