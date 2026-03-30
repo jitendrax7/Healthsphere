9afe7fe133653bde8b7602fd948ee19bd5453290
jitendrax7 <rathourjitendrasingh472@gmail.com>
2026-03-30 09:36:48 +0530

    k

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
-import dotenv from "dotenv";
-dotenv.config();
-// console.log("GROQ KEY:", process.env.GROQ_API_KEY);
+import "./config/env.js";
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/users/userRoutes.js";
 import authRoutes from "./routes/auth/authRoutes.js";
 import docterRoutes from "./routes/docters/doctorRoutes.js";
+import hospitalRoutes from "./routes/hospital/hospitalRoutes.js";
 
 connectDB();
 
@@ -14,26 +13,22 @@ const app = express();
 
 app.use(cors());
 
-// app.use(cors({
-//   origin: "http://localhost:3000", // your frontend URL
-//   credentials: true
-// }));
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("HealthSphere API Running...");
 }); 
-
+  
 
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/doctor", docterRoutes);
-
+app.use("/api/hospital", hospitalRoutes)
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+    
