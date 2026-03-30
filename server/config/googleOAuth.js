8856581ad1d6eb2e2d62f6ebcb663a02265c4657
jitendrax7 <rathourjitendrasingh472@gmail.com>
2026-03-30 09:40:08 +0530

    add config

---
+++ server/config/googleOAuth.js
@@ -0,0 +1,19 @@
+import { google } from "googleapis";
+
+export const oauth2Client =
+    new google.auth.OAuth2(
+
+        process.env.GOOGLE_CLIENT_ID,
+
+        process.env.GOOGLE_CLIENT_SECRET,
+
+        process.env.GOOGLE_REDIRECT_URI
+
+    );
+
+export const GOOGLE_SCOPES = [
+
+    'https://www.googleapis.com/auth/calendar',
+    'https://www.googleapis.com/auth/userinfo.email'
+
+];
